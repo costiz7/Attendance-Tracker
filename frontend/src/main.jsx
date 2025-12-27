9631dd4e742c87caf3c71b09cacf58cc63452178
Dedelene <voicudiana66@gmail.com>
2025-12-27 16:56:47 +0200

    snowfall mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Snowfall from 'react-snowfall';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Snowfall color="#82C3D9"/>
     <App />
   </StrictMode>,
 )
